da21ab187a8583848e90b0cfb46687a97f21e1da
bordedelcielo <christopher.thrutchley@gmail.com>
2022-10-02 00:19:05 -0500

    message

--- nba-data.py
+++ nba-data.py
@@ -1,1 +1,11 @@
 import pandas as pd
+import numpy as np
+
+nba_data = pd.read_csv('nbastats2018-2019.csv')
+print(nba_data)
+
+print(nba_data.columns)
+
+print(nba_data['FG3%'])
+
+print(nba_data.sort_values('FG3%', ascending = False))
